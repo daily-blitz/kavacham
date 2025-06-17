827b6f8710c617bbb52087ebdb0b3ce14bc3b56b
Akshay Shah <akshay27991@gmail.com>
2025-06-18 00:30:12 +0530

    Add classic customer mutations (unused with new API)

---
+++ app/graphql/storefront/CustomerMutations.ts
@@ -0,0 +1,67 @@
+// Classic Customer Account Mutations using Storefront API
+
+export const CUSTOMER_ACCESS_TOKEN_CREATE = `#graphql
+  mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
+    customerAccessTokenCreate(input: $input) {
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+` as const;
+
+export const CUSTOMER_CREATE = `#graphql
+  mutation customerCreate($input: CustomerCreateInput!) {
+    customerCreate(input: $input) {
+      customer {
+        id
+        email
+        firstName
+        lastName
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+` as const;
+
+export const CUSTOMER_RECOVER = `#graphql
+  mutation customerRecover($email: String!) {
+    customerRecover(email: $email) {
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+` as const;
+
+export const CUSTOMER_RESET = `#graphql
+  mutation customerReset($id: ID!, $input: CustomerResetInput!) {
+    customerReset(id: $id, input: $input) {
+      customer {
+        id
+        email
+      }
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+` as const;
