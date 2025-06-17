4208c154b795d4cdfb43beab021ec5aff04a4b68
Akshay Shah <akshay27991@gmail.com>
2025-06-18 00:32:22 +0530

    Remove duplicate account routes

--- app/routes/account.login.tsx
+++
@@ -1,9 +0,0 @@
-import {redirect, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
-
-export async function loader({context}: LoaderFunctionArgs) {
-  return context.customerAccount.login();
-}
-
-export default function Login() {
-  return null;
-}
